b7638ad255a1e689ed4bda05ce6e2857c3560377
Rene Saarsoo <nene@triin.net>
2012-08-13 18:07:06 +0300

    Extract examples for JSDuck export JSON instead.
    For start just extracting from the class doc.

--- app.js
+++ app.js
@@ -44,66 +44,55 @@ function indent(spaces, text) {
     return text.replace(/^/gm, spaces);
 }
 
+function extractSnippets(html) {
+    // Create a non-greedy regular expression to strip out any <pre> blocks.
+    var re = new RegExp("<pre><code>(.)*?</code></pre>", "ig");
+    return html.match(re) || [];
+}
+
 // Counters for successes and failures
 var totalFailures = 0;
 var totalSuccesses = 0;
 
 // Loop over each of the file names in the specified directory.
 FILES.forEach(function (file) {
-    // Read the specified file and try and strip the JSONP callback.
-    var content = fs.readFileSync(file, "utf-8").match(/\((.*)\)/g)[0];
-    content = content.substring(1, content.length - 1); // strip the first and last character.
-
-    // Convert the file contents from a string to a JSON object.
-    var jsonContent = JSON.parse(content);
-
-    // Strip out the HTML content for easier reading/logging.
-    // <debug>
-    // jsonContent.html = "";
-    // </debug>
-
-    // Replace all newlines with some junk character.
-    jsonContent.html = jsonContent.html.replace(/\n/g, "\007");
-
-    // Create a non-greedy regular expression to strip out any <pre> blocks.
-    var re = new RegExp("<pre><code>(.)*?</code></pre>", "ig"),
-    mat = jsonContent.html.match(re);
-
     console.log(file + ":");
-    // If we have any RegExp matches...
-    if (mat) {
-        // Loop over each <pre><code>...</code></pre> code block...
-        mat.forEach(function (item, idx) {
-            // Strip out any HTML content and convert all our junk characters back to a newline.
-            var str = stripHtml(item.replace(/\007/g, "\n"));
 
-            // Lets JSHint the contents and see if it is valid JavaScript.
-            // Note: It may not even be JavaScript at all, who knows. Exciting!
-            if (!jshint(str)) {
-                // If we have errors, display the example number (1-based), just for giggles.
-                console.log("  - Example #%d: FAIL :(", idx + 1);
-                // Increment the global failures count.
-                totalFailures++;
-                if (VERBOSE) {
-                    // Loop over each error and display the line number and JSHint warning message.
-                    jshint.errors.forEach(function (error) {
-                        if (error) {
-                            console.log("    line: " + error.line + ", reason:" + error.reason);
-                        }
-                    });
-                    console.log("    ------------");
-                    console.log(indent("    ", str));
-                    console.log("    ------------");
-                }
-            } else {
-                console.log("  - Example #%d: OK", idx + 1);
-                totalSuccesses++;
+    // Read the JSON from file
+    var json = JSON.parse(fs.readFileSync(file, "utf-8"));
+
+    // check the documentation of a class itself
+    var mat = extractSnippets(json.doc);
+
+    // Loop over each <pre><code>...</code></pre> code block...
+    mat.forEach(function (item, idx) {
+        // Strip out any HTML content and convert all our junk characters back to a newline.
+        var str = stripHtml(item.replace(/\007/g, "\n"));
+
+        // Lets JSHint the contents and see if it is valid JavaScript.
+        // Note: It may not even be JavaScript at all, who knows. Exciting!
+        if (!jshint(str)) {
+            // If we have errors, display the example number (1-based), just for giggles.
+            console.log("  - Example #%d: FAIL :(", idx + 1);
+            // Increment the global failures count.
+            totalFailures++;
+            if (VERBOSE) {
+                // Loop over each error and display the line number and JSHint warning message.
+                jshint.errors.forEach(function (error) {
+                    if (error) {
+                        console.log("    line: " + error.line + ", reason:" + error.reason);
+                    }
+                });
+                console.log("    ------------");
+                console.log(indent("    ", str));
+                console.log("    ------------");
             }
+        } else {
+            console.log("  - Example #%d: OK", idx + 1);
+            totalSuccesses++;
+        }
 
-        });
-    } else {
-        console.log("    No [inline] examples found, congrats!");
-    }
+    });
 
     /*
     // Alias finder.
